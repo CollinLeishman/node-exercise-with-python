dfdd099c5238ed0ad6161cbefcf62775de84f7f1
Collin Leishman <collin.leishman@hammerspace.com>
2022-07-05 11:14:10 -0600

    Add usage to readme, return responses with json so they're formatted.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # node-exercise-with-python
+
+You'll need to have python3 installed.
+```
+python3 -m venv venv
+source ./venv/bin/activate
+# verify pip3 is in virtualenv
+which pip3
+pip3 install -r requirements.txt
+python3 app.py
+```
+Then you can either visit the GUI or open another terminal and run:
+```
+curl http://127.0.0.1:5000/people
+curl http://127.0.0.1:5000/people?sortBy=height
+curl http://127.0.0.1:5000/people?sortBy=name
+curl http://127.0.0.1:5000/people?sortBy=mass
+curl http://127.0.0.1:5000/planets
+```
--- app.py
+++ app.py
@@ -2,18 +2,25 @@
 
 """
 Requirements: Create /people and /planets endpoints.
-Contacting swapi returns a json object with 4 keys: count, next, previous, and results. We shouldn't need any besides results, since previous and next are not required if all are returned at once.
+Contacting swapi returns a json object with 4 keys: count, next, previous, and
+results. We shouldn't need any besides results, since previous and next are
+not required if all are returned at once.
 
 /people endpoint
-Should contact swapi and get all people, the swapi doesn't return all at once. It has a key "next" with the value of the next url we want to contact. Allow optional arg "sortBy", which can be set to name, height, or mass, and return the json object sorted according to the argument.
+Should contact swapi and get all people, the swapi doesn't return all at once.
+It has a key "next" with the value of the next url we want to contact. Allow
+optional arg "sortBy", which can accept parameters name, height, or mass, and
+return the json object sorted according to the parameter provided.
 
 /planets endpoint
-Should contact the swapi and return all planets, and should have the resident names rather than URLs to the resident info.
+Should contact the swapi and return all planets, and should have the resident
+names rather than URLs to the resident info.
 """
 
-from flask import Flask
+from flask import Flask, request
 import requests
-import json
+# For some reason jsonify still dumps an ugly response.
+from json import dumps
 
 app = Flask(__name__)
 
@@ -23,7 +30,8 @@ def show_planets():
     """
     First: Want to get all of the planets into one dictionary.
     Second: Want to loop over all planets and change the value of "residents"
-    to include names rather than urls.
+    to include names rather than urls, and put that in another dictionary, and
+    return that.
     """
 
     all_planets = {"results": []}
@@ -34,13 +42,19 @@ def show_planets():
         for planet in planets:
             all_planets["results"].append(planet)
         planets_response = requests.get(next_url)
-    # Done with first requirement. all_planets["results"] should be a list of dictionaries.
+    # Done with Afirst requirement.
 
-    # updated_planets is what we want to return to the user. It should be a dict with the resident names rather than than urls to resident info.
+    """
+    updated_planets is what we want to return to the user. It should be a dict
+    with the resident names rather than than urls to resident info.
+    """
     updated_planets = {"results": []}
     for planet in all_planets["results"]:
-        # Add the planet to the dict we want to return. Change the value of residents in that dict. Return as a json.
-        # list of urls to residents
+        """
+        For each planet, create a list of resident names, then change the value
+        of planets["residents"] to that. Then add the updated planet info to
+        updated_planets.
+        """
         resident_urls = planet["residents"]
         resident_names = []
         for url in resident_urls:
@@ -50,26 +64,44 @@ def show_planets():
             planet["residents"] = resident_names
             updated_planets["results"].append(planet)
 
-    return updated_planets
+    return dumps(updated_planets, indent=3)
 
 
 @app.route('/people')
 def show_people():
-    # The default response from swapi only returns 10 people, and has the following keys: count, next, previous, results. Since we are getting all 82 in one response, we only need the results key.
+    """
+    First: Get all people in a list, make that list the value of a dictionary.
+    Second: If the user requested the list be sorted by mass, height, or name,
+    then return the sorted list
+    """
     all_people = {"results": []}
     response = requests.get("https://swapi.dev/api/people")
     while response.json()["next"] is not None:
         next_url = response.json()["next"]
-        # Re-define response so that by time we're back to the if statement we have a new val for response.json()["next"].
         response = requests.get(next_url)
-        all_people["results"].append(response.json())
+        for person in response.json()["results"]:
+            all_people["results"].append(person)
 
-    return all_people
+    # Now we want to see if they requested sortBy=*.
+    sort_by = request.args.get("sortBy")
+    if sort_by is not None:
+        unsorted_people = all_people["results"]
+        supported_sort_params = ["mass", "height", "name"]
+        if sort_by not in supported_sort_params:
+            return 'Unable to sort by the parameter %s.' % sort_by, 400
+        else:
+            # Sort the list by the provided parameter.
+            sorted_list = (sorted(unsorted_people, key=lambda i: i[sort_by]))
+            all_people["results"] = sorted_list
+            return dumps(all_people, indent=3)
+
+    else:
+        return dumps(all_people, indent=3)
 
 
 @app.route('/')
-def wrong_path():
-    return "You're in the wrong path"
+def show_usage():
+    return "Usage:\ncurl http://127.0.0.1:5000/people?sortBy=mass\ncurl http://127.0.0.1:5000/planets\n"
 
 
 if __name__ == '__main__':
